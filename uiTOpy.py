9a969ee311108acd2519b0099e4a3fa631c6597f
cober2018 <40491174+cober2018@users.noreply.github.com>
2018-06-23 13:45:44 +0800

    Add files via upload

---
+++ uiTOpy.py
@@ -0,0 +1,30 @@
+import os
+import os.path
+
+#UI文件所在的路径
+dir = "./"
+
+#列出目录下的所有UI文件
+def listUiFile():
+    list = []
+    files = os.listdir(dir)
+    for filename in files:
+        if os.path.splitext(filename)[1] == '.ui':
+            list.append(filename)
+
+    return list
+
+
+#把扩展名为.ui的文件改成扩展名为.py的文件
+def transPyFile(filename):
+    return os.path.splitext(filename)[0] + '.py'
+#调用系统命令把ui文件转化成python文件
+def runMain():
+    list = listUiFile()
+    for uifile in list:
+        pyfile = transPyFile(uifile)
+        cmd = 'pyuic5 -o {pyfile}{uifile}'.format(pyfile=pyfile,uifile=uifile)
+        #print(cmd)
+        os.system(cmd)
+if __name__ == '__main__':
+    runMain()
